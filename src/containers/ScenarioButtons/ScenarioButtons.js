125d8e84e89b38ea10ba068a8743f2f8a00a6928
Erika <adob.e@hotmail.com>
2024-05-04 09:04:00 +0200

    Ajoute les differents textes pour les boutons

--- src/containers/ScenarioButtons/ScenarioButtons.js
+++ src/containers/ScenarioButtons/ScenarioButtons.js
@@ -1,11 +1,21 @@
 import style from './ScenarioButtons.module.css';
 
 export default function ScenarioButtons({addStep, removeStep, step, max}) {
+
+    const nexButton = [
+        "C'est parti !",
+        "C'était facile",
+        "J'ai comprit !",
+        "OK !",
+        "Bizarre cette histoire...",
+        "C'est quoi la suite ? ",
+        "Trop cool !"
+    ]
     return (
     <div className={style.buttonsContainer}>
         <div className={style.previousNextContainer}>
             {step > 0 && <button className="button" onClick={removeStep}><i className="fa-solid fa-arrow-left"></i>Etape précédente</button>}
-            {step < max -1 && <button className="button" onClick={addStep}>Prochaine étape<i className="fa-solid fa-arrow-right"></i></button>}
+            {step < max -1 && <button className="button" onClick={addStep}>{nexButton[step]}<i className='fa-solid fa-arrow-right'></i></button>}
         </div>
     </div>
     )
